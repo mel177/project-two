ff889cfb7266e1b238cc61a04bf628db5a01bb35
Melinh Mai <melinh.mai@gmail.com>
2018-08-15 17:41:44 -0500

    studentModel

--- models/tutorsModule.js
+++ models/tutorsModule.js
@@ -27,31 +27,4 @@ module.exports = function (sequelize, DataTypes) {
     }
   });
   return Tutors;
-
-
-  var Students = sequelize.define("Students", {
-
-    name: {
-      type: DataTypes.STRING
-    },
-
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-
-    phone: {
-
-      type: DataTypes.INTEGER,
-
-    }
-  });
-  return Students;
-
-
 };
