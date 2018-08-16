3c4611715d2304840cadee6588bbb87953b0416f
Bommala <basusmy@gmail.com>
2018-08-15 21:02:37 -0500

    front end JS

---
+++ public/js/student.js
@@ -0,0 +1,126 @@
+var selectedValue;
+var tutorModal = $('#TBD');
+var tutorModalBody = $('#TBD');
+
+$(document).ready(function() {
+  
+  $('#TBD').on("change", handleTutorFormSubmit);
+ 
+  $('#TBD').on("change", handleSubjectFormSubmit);
+
+
+    function handleTutorFormSubmit(event) {
+    event.preventDefault();
+  
+    var selectBox = document.getElementById("#TBD");
+    selectedValue = selectBox.options[selectBox.selectedIndex].value;
+
+    if (!selectedValue) {
+      return;
+    } else {
+      parseTutorData(allTutors);
+
+    }
+  
+  };
+
+  //use userId to collect relevant information for tutor and render in modal
+  //then run function to match userId to subject table and display subjects in modal
+
+  function parseTutorData (data) {
+      console.log(selectedValue);
+      for (var i = 0; i < allTutors.length; i++) {
+        if (selectedValue == parseInt(allTutors[i].id)) {
+          $('title').html(allTutors[i].firstName);
+          $('body').html("#TBD " + allTutors[i].about + "</p>");
+          $('photo').html("#TBD" + allTutors[i].id + ".jpg'>");
+          $('footer').html("#TBD" + allTutors[i].id + "?_method=PUT' method='POST'><input type = 'hidden' name = 'available' value = '0'><button type='submit' class='btn btn-info btn-primary CRUDSubmit'>Book A Session With " + allTutors[i].firstName+"</button></form>")
+          
+          matchTutorSubjects(allTutors[i].SubjectId);
+
+          showModal();
+         }    
+      }
+  }
+
+  //match userId to subject table
+
+  function matchTutorSubjects (data) {
+    
+    for (var j = 0; j < allSubjects.length; j++) {
+      
+      if (data == allSubjects[j].id) {
+        // console.log(allSubjects[j].subjectName)
+        $('#TBD').html("Subject " + allSubjects[j].subjectName + "</h4>");
+      }
+    }
+  }
+
+  //display modal with specific tutor information
+  function showModal(){
+    tutorModal.modal('show');    
+  }
+
+
+
+// Subject Button
+
+var selectedValue2;
+var userIdSubjTutor = [];
+var tutorRow = "";
+
+//reset state
+
+  function resetTutorsBySubject(){
+    selectedValue2 = 0;
+    userIdSubjTutor = [];
+    tutorRow = "";
+  }
+
+//take in subjectID from dropdown menu
+  function handleSubjectFormSubmit(event) {
+      event.preventDefault();
+      // console.log(allSubjects);
+      var selectBox2 = document.getElementById("#TBD");
+      selectedValue2 = selectBox2.options[selectBox2.selectedIndex].value;
+
+      if (!selectedValue2) {
+        return;
+      }
+      
+      parseTutorBySubject(allTutors)
+      resetTutorsBySubject();
+
+  }
+
+//parse object of allTutors to find out where userID has relevant subjectID
+//run function to create row of tutors with relevant subjectID
+  function parseTutorBySubject (data) {
+    console.log(selectedValue2);
+      for (var i = 0; i < allTutors.length; i++) {
+        // console.log(parseInt(allTutors[i].SubjectId))
+        if (parseInt(selectedValue2) === parseInt(allTutors[i].SubjectId)) {
+          userIdSubjTutor.push(allTutors[i]);
+         }    
+      }
+    createTutorRow(userIdSubjTutor);
+  }
+
+//function to create row of tutors with relevant subjectID
+  function createTutorRow(data) {
+      for (var i = 0; i < data.length; i++) {
+        tutorRow += "#TBD " + data[i].firstName+"#TBD"
+      }
+      
+      showTutorRow(tutorRow); 
+
+    }
+
+// display the row of relevant tutors by subject
+  function showTutorRow(data) {
+     $('#TBD').html(data);
+  }
+
+
+
+}); 
---
+++ public/js/tutor.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+  // Getting references to the name inout and author container, as well as the table body
+  var nameInput = $("#tbd").val().trim();
+  var usernameInput = $("#tbd").val().trim();
+  var passwordInput = $("#tbd").val().trim();
+  var phoneInput = $("#tbd").val().trim();
+  var subjectsInput = $("#tbd").val().trim();
+  var bioInput = $("#tbd").val().trim();
+  var ratingsInput = $("#tbd").val().trim();
+  var availabilityInput = $("#tbd").val().trim();
+  
+  // Adding event listeners to the form to create a new object, and the button to delete
+  // an Author
+  $(document).on("submit", "#tbd", handleUserFormSubmit);
+  
+
+  // A function to handle what happens when the form is submitted to create a new Author
+  function handleUserFormSubmit(event) {
+    event.preventDefault();
+    // Don't do anything if the name fields hasn't been filled out
+    if (!firstNameInput || lastNameInput || emailInput || aboutInput || subjectInput) {
+      return;
+    }
+    // Calling the insertUser function and passing in the value of the name input
+    insertUser({
+      name: nameInput,
+      username: usernameInput,
+      password: passwordInput,
+      phone: phoneInput,
+      Subjects: subjectsInput,
+      bio: bioInput,
+      ratings: ratingsInput,
+      availability: availabilityInput
+
+    });
+  }
+
+  // A function for creating an author. Calls getAuthors upon completion
+  function insertUser(userData) {
+    $.post("/api/tutors", userData)
+      //.then(getAuthors);
+  }
+
+  // Function for retrieving authors and getting them ready to be rendered to the page
+  function getAuthors() {
+    $.get("/api/authors", function(data) {
+      var rowsToAdd = [];
+      for (var i = 0; i < data.length; i++) {
+        rowsToAdd.push(createAuthorRow(data[i]));
+      }
+      renderAuthorList(rowsToAdd);
+      nameInput.val("");
+    });
+  }
+
+  
+
+  
+
+  
+});
