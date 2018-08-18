8ccbc0d47ab4911845b69f594dfb4e7a286edc53
Bommala <basusmy@gmail.com>
2018-08-18 00:22:28 -0500

    login authentication created and routes updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,63 +1,43 @@
 var db = require("../models");
 
-
-module.exports = function(router) {
-
-  // Find all Tutors with the subject searched for and return them to the user with res.json
-  router.get("/api/tutors", function(req, res) {
-    db.Tutors.findAll({
-        where: {
-            subject: req.body.subject
-        }
-    }).then(function(dbTutors) {
-        //res.json(dbTutor)
-      //res.render('index', dbTutors);
+module.export = function(app){
+    
+    //find all tutors
+  app.get("/api/tutors", function(req, res) {
+    db.Tutors.findAll({}).then(function(dbTutors) {
+        res.json(dbTutor)
     });
   });
 
-  // Create a new tutor with the data available to us in req.body from the sign up page
-  router.post("/create", function(req, res) {
-    //console.log(req.body);
-    db.Tutors.create({
-            name: req.body.name,
-            username: req.body.username,
-            password: req.body.password,
-            bio: req.body.bio,
-            phonenumber: req.body.number,
-            subject: req.body.subject
-    }).then(function(dbTutor) {
-      res.json(dbTutor);
-    });
-  });
+//find specific tutor
+  app.get("/api/tutors/:id", function(req, res) {
 
-//Create a new student with the data avaiable to us in req.body from the sign up form
-  router.post("/create", function(req, res) {
-    //console.log(req.body);
-    db.Students.create({
-            name: req.body.name,
-            username: req.body.username,
-            password: req.body.password,
-            phonenumber: req.body.number        
-    }).then(function(dbTutor) {
-      res.json(dbTutor);
-    });
-  });
-
-  router.post("/update", function (req, res){
-    db.Tutors.update({
+    db.tutors.findAll({
       where: {
-        id: req.body.id
+        id: req.params.id
       }
-    })
-  })
+    }).then(function(dbTodo) {
+      res.json(dbTodo);
+    });
 
+  });
 
-  // Delete the Tutor with the id available to us in req.params.id
-  router.delete("/api/tutors/delete", function(req, res) {
-    db.Tutor.destroy({ where: { id: req.body.id }}).then(function(dbTutor) {
-      res.json(dbTutor);
+  // find all students
+  app.get("/api/students", function(req, res) {
+    db.students.findAll({}).then(function(dbStudents) {
+        res.json(dbStudents)
     });
   });
 
-};
+//find specific student
+app.get("/api/students/:id", function(req, res) {
 
+  db.students.findAll({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(dbStudents) {
+    res.json(dbStudents);
+  });
+})
+};
