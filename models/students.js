c9b61dec6e2ab96010839434f5536dcb6de08921
Melinh Mai <melinh.mai@gmail.com>
2018-08-20 20:08:17 -0500

    fix

--- models/students.js
+++ models/students.js
@@ -1,37 +1,38 @@
 module.exports = function(sequelize, DataTypes) {
 
-    var Student = sequelize.define("Student", {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
-        },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        },
-      username: {
-        type:DataTypes.STRING,
-        //allowNull: false,
-        },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
-        },
-        classMethods: {
-          associate: function(models) {
-            Student.belongsTo(models.User, {
-              foreignKey: {
-                allowNull: false
-                }
-              });
-            Student.hasMany(models.Appointment, {
-              onDelete: "cascade"
-              });
-            }
+  var Student = sequelize.define("Student", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+      },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      },
+    username: {
+      type:DataTypes.STRING,
+      //allowNull: false,
+      },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+      },
+    }, {
+      classMethods: {
+        associate: function(models) {
+          Student.belongsTo(models.User, {
+            foreignKey: {
+              allowNull: false
+              }
+            });
+          Student.hasMany(models.Appointment, {
+            onDelete: "cascade"
+            });
           }
-        });
-    return Student;
-  
-  };
+        }
+      });
+  return Student;
+
+};
