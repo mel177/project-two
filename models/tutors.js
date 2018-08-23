166c63162acc9c6af06095d83a94ce68df02d508
Melinh Mai <melinh.mai@gmail.com>
2018-08-22 19:45:02 -0500

    seeds

--- models/tutors.js
+++ models/tutors.js
@@ -29,7 +29,7 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.TEXT
         },
         photo: {
-            type: DataTypes.BLOB,
+            type: DataTypes.BLOB
           },
     
         ratings: {
